a69f5aeb553b610b8d2bc3657754b848e495040a
Annika von Bayern <annikawespe@gmail.com>
2025-09-14 17:22:36 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+
+module.exports = {
+  content: ["./*.html", "./pages/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        background: '#181818',
+        primary: '#B23EB6',
+        secondary: '#D19ACD',
+        cream: '#f6f2e5',
+        header: '#1f1c1c'
+      },
+    },
+  },
+  plugins: [],
+}
