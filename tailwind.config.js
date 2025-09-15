306049a29b9b7016fa8fca3d0c8fc49931f3b7bb
Annika von Bayern <annikawespe@gmail.com>
2025-09-15 17:53:49 +0200

    startpage quasi fertig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,20 @@
-
 module.exports = {
   content: ["./*.html", "./pages/*.html"],
   theme: {
     extend: {
       colors: {
-        background: '#181818',
-        primary: '#B23EB6',
-        secondary: '#D19ACD',
-        light_background: '#c8c9d1',
-        creme: 'f6f2e5',
-        headingAccent: '#8a82b0',
-        header: '#1f1c1c'
+        background: "#181818",
+        header_background: "#1f1c1c",
+        light_background: "#c8c9d1",
+        primary: "#B23EB6",
+        secondary: "#D19ACD",
+        creme: "#f6f2e5",
+        boxDark: "#4B2D5C",
+        boxMid: "#845CA8",
+        boxLight: "#D9C8E8",
+        headingAccent: "#8a82b0",
       },
     },
   },
   plugins: [],
-}
+};
