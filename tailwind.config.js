9d74286a49bf05ebc5ec0cce906437867eeee0c0
Annika von Bayern <annikawespe@gmail.com>
2025-09-19 18:15:24 +0200

    text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,7 @@ module.exports = {
         headingAccent: "#8a82b0",
         headingAccentDark: "#544c78",
         brighterBackground: "#D19ACD",
-        textOnBlack: "#f6f2e5",
+        textOnBlack: "#8a82b0",
         textOnBrighterBackground: "#3A2246",
         textAccentOnBrighterBackground: "#943fa7"
       },
