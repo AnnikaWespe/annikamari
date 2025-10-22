cc47b6ddbdd780cfbedc47c47a648958679bc172
Annika von Bayern <annikawespe@gmail.com>
2025-10-22 20:38:27 +0200

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,20 +3,23 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        background: "#181818",
-        header_background: "#1f1c1c",
+        background: "#020127",
+        header_background: "#020130",
         light_background: "#c8c9d1",
         primary: "#B23EB6",
         secondary: "#D19ACD",
+        secondaryDarkened: "#ac4ea5",
         creme: "#f6f2e5",
-        boxDark: "#4B2D5C",
+        textButtonFrontPage: "#4B2D5C",
+        buttonHoverFrontpage: "#000b43",
         boxMid: "#845CA8",
         boxLight: "#D9C8E8",
+        boxDark: "#758BFF",
         headingAccentDark: "#544c78",
         brighterBackground: "#f3e5f2",
         textOnBlack: "#9f98be",
         textOnBrighterBackground: "#3A2246",
-        textAccentOnBrighterBackground: "#943fa7"
+        textAccentOnBrighterBackground: "#943fa7",
       },
     },
   },
